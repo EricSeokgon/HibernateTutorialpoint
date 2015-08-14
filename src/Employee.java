78863d8cb5646cb10c5e0d94a1052e3a9c3acaa7
Hadeslee <leesk55@gmail.com>
2015-08-15 07:40:05 +0900

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,46 @@
+/**
+ * Project: HibernateTutorial
+ * FileName: Employee
+ * Date: 2015-08-15
+ * Time: 오전 7:38
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+
+    public Employee() {}
+    public Employee(String fname, String lname, int salary) {
+        this.firstName = fname;
+        this.lastName = lname;
+        this.salary = salary;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId( int id ) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName( String first_name ) {
+        this.firstName = first_name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName( String last_name ) {
+        this.lastName = last_name;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    public void setSalary( int salary ) {
+        this.salary = salary;
+    }
+}
